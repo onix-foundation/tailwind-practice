d947689bf042e3c472d7af1860124e3a06faba5d
Joshua Prpich <joshuaprpich@gmail.com>
2022-01-19 10:13:48 -0600

    extend tailwind.config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF6363",
+        secondary: {
+          100: "#E2E2D5",
+          200: "#888883",
+        },
+      },
+    },
   },
   plugins: [],
 };
